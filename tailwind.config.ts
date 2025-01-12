1354247c198432977494a1b83d57cc2296d93aee
arbusam <arhan.busam@gmail.com>
2025-01-13 10:17:53 +1100

    Basic file upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
+/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "media",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,5 +18,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 } satisfies Config;
